10472d86a8deeb808d2a0884414705d5d712c861
CMontesino <cmontesinomartos@alumnat.iessacolomina.es>
2024-05-17 16:31:26 +0200

    Add files via upload

---
+++ El JS.js
@@ -0,0 +1,33 @@
+function a() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Indice').innerHTML;
+}
+function b() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Casos').innerHTML;
+}
+function c() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Especificaciones').innerHTML;
+}
+function d() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Especificaciones 2').innerHTML;
+}
+function e() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Presentacion').innerHTML;
+}
+function f() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Funcionamiento').innerHTML;
+}
+function g() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Escaneo').innerHTML;
+}
+function h() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Recuperacion').innerHTML;
+}
+function i() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Guardado').innerHTML;
+}
+function j() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Alternativas').innerHTML;
+}
+function k() {
+    document.getElementById('Portada').innerHTML = document.getElementById('Valoracion').innerHTML;
+}
